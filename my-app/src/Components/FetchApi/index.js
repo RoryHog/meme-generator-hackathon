b8490e3d9fc14c5ecca721e0877be5c817527de5
Abdullahi <amohamoud1999@gmail.com>
2022-06-17 14:15:01 +0100

    added inputs

--- my-app/src/Components/FetchApi/index.js
+++ my-app/src/Components/FetchApi/index.js
@@ -27,12 +27,36 @@ import Button from "../Button";
         setMeme({randomImage: url})
         
     }
+
+    // create a function to get the value inside the input boxes (e.target.value)
+    //create a useState and set the state to the information inputted
+    //display text inside a p tag
+    //position inputs and p tag 
     
+    const [text, setText] = useState("")
+    const[text2, setText2] = useState("")
+    const [memeText, setMemeText] = useState([])
+
+    function getFirstInput(e) {
+        setText(e.target.value)
+    }
+    function getSecondInput(e) {
+        setText2(e.target.value)
+    }
+
+    function handleClick(){
+        setMemeText([...memeText, text])
+    }
 
     return (
         <>
         <Button onClick={getMemeImage}/>
-        <img className="image" src={meme.randomImage} alt={meme.name} />
+        <p>{text}</p>
+        <p>{text2}</p>
+        <button onClick={handleClick}>add text</button>
+        <input onInput={getFirstInput} className="top-line" placeholder="Top Line"></input>
+        <input onInput={getSecondInput} className="bottom-line" placeholder="Bottom Line"></input>
+        <img className="image" src={meme.randomImage} alt={meme.name}/>
         </>
     )
  }
